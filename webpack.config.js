5b4130f5be72689f301be44f9a011e686c282bd7
Dejan Stojadinovic <dejanstojadinovic@outlook.com>
2018-03-18 21:25:37 +0100

    webpack config for deploying to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
 const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-      }, {
-          loader: "css-loader" // translates CSS into CommonJS
-      }, {
-          loader: "sass-loader" // compiles Sass to CSS
-      }]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+module.exports = env => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: ['babel-polyfill', './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: 'style-loader' // creates style nodes from JS strings
+            },
+            {
+              loader: 'css-loader' // translates CSS into CommonJS
+            },
+            {
+              loader: 'sass-loader' // compiles Sass to CSS
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin()
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  };
 };
